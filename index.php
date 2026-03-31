e08c3f36ebed049a4e38b7a4aa57b1c40eb75a36
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2026-03-31 12:05:27 +0100

    update

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 include "../base/chech.php"; 
 include "../base/main.php";
 include "db.php"
